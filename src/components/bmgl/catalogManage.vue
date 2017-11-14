10b526645508b29f260a65c136e338a7f4d0ef5f
shiyiqiao <1481051940@qq.com>
2017-11-14 17:43:59 +0800

    tijiao

--- src/components/bmgl/catalogManage.vue
+++ src/components/bmgl/catalogManage.vue
@@ -1,8 +1,33 @@
 <template>
-
+  <div class="subContent">
+    <div class="box-left">
+      <ul class="ztree" id="tree"></ul>
+    </div>
+    <div class="box-right"></div>
+  </div>
 </template>
+
 <script>
+
   export default{
     name:'catalogManage'
   }
 </script>
+<style>
+  .subContent{
+    position: relative;
+    background: #ffffff;
+    border: 1px solid #ddd;
+    clear:both;
+  }
+  .subContent .box-left{
+    position: absolute;
+    width: 250px;
+    height: 100%;
+  }
+  .subContent .right-box{
+    width: 100%;
+    padding-left: 250px;
+    height: 100%;
+  }
+</style>
